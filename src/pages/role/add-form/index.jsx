dce54ca2850bf4827499c6a77ef9c9bd11810d70
a1104691426 <1104691426@qq.com>
2021-11-25 19:47:10 +0800

    Role的添加修改

--- src/pages/role/add-form/index.jsx
+++ src/pages/role/add-form/index.jsx
@@ -1,47 +1,40 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
+import { Form, Input } from "antd";
 
-import { Form , Input } from "antd";
+const Item = Form.Item;
+
 /*
 添加分类的form组件
-*/
-const Item = Form.Item;
+ */
 class AddForm extends Component {
+  static propTypes = {
+    setForm: PropTypes.func.isRequired, // 用来传递form对象的函数
+  };
 
-  static propsTypes = {
-    setForm:PropTypes.func.isRequired,
-    catergorys:PropTypes.array.isRequired,
-    parentId:PropTypes.string.isRequired
+  componentWillMount() {
+    this.props.setForm(this.props.form);
   }
-  
-  componentWillMount(){
-    //将form对象通过setForm()传递父组件
-    this.props.setForm(this.props.form)
 
-  }
   render() {
     const { getFieldDecorator } = this.props.form;
+    // 指定Item布局的配置对象
     const formItemLayout = {
-        labelCol: { span: 5 },
-        wrapperCol: { span: 15 },
-      };
+      labelCol: { span: 4 }, // 左侧label的宽度
+      wrapperCol: { span: 15 }, // 右侧包裹的宽度
+    };
+
     return (
       <Form>
-        <Item label='角色名称' {...formItemLayout}>
-            {getFieldDecorator('roleName',{
-                initialValue:'',
-                rules:[
-                  {
-                    required:true,
-                    message:'角色名称必须输入'
-                  }
-                ]
-            })(
-                <Input placeholder="请输入角色名称" />
-            )}
+        <Item label="角色名称" {...formItemLayout}>
+          {getFieldDecorator("roleName", {
+            initialValue: "",
+            rules: [{ required: true, message: "角色名称必须输入" }],
+          })(<Input placeholder="请输入角色名称" />)}
         </Item>
       </Form>
     );
   }
 }
+
 export default Form.create()(AddForm);
--- src/pages/role/index.jsx
+++ src/pages/role/index.jsx
@@ -1,18 +1,30 @@
 import React, { Component } from "react";
 import { Card, Button, Table, Modal, message } from "antd";
-
 import { PAGE_SIZE } from "../../utils/constants";
-import { reqRoles } from "../../api";
+import { reqRoles, reqAddRole, reqUpdateRole } from "../../api";
 import AddForm from "./add-form";
-import {reqAddRole} from '../../api'
+import AuthForm from "./auth-form";
+import memoryUtils from "../../utils/memoryUtils";
+import { formateDate } from "../../utils/dateUtils";
+import storageUtils from "../../utils/storageUtils";
 
-//角色路由
+/*
+角色路由
+ */
 export default class Role extends Component {
   state = {
-    roles: [], //所有角色的列表
-    role: {}, //选中的role
-    isShowAdd: false, //是否显示添加界面
+    roles: [], // 所有角色的列表
+    role: {}, // 选中的role
+    isShowAdd: false, // 是否显示添加界面
+    isShowAuth: false, // 是否显示设置权限界面
   };
+
+  constructor(props) {
+    super(props);
+
+    this.auth = React.createRef();
+  }
+
   initColumn = () => {
     this.columns = [
       {
@@ -22,10 +34,12 @@ export default class Role extends Component {
       {
         title: "创建时间",
         dataIndex: "create_time",
+        render: (create_time) => formateDate(create_time),
       },
       {
         title: "授权时间",
         dataIndex: "auth_time",
+        render: formateDate,
       },
       {
         title: "授权人",
@@ -33,70 +47,137 @@ export default class Role extends Component {
       },
     ];
   };
+
   getRoles = async () => {
     const result = await reqRoles();
     if (result.status === 0) {
       const roles = result.data;
-      this.setState({ roles });
+      this.setState({
+        roles,
+      });
     }
   };
+
   onRow = (role) => {
     return {
       onClick: (event) => {
-        console.log(role);
+        // 点击行
+        console.log("row onClick()", role);
+        // alert('点击行')
         this.setState({
           role,
         });
       },
     };
   };
-  addRole =() => {
-    this.form.validateFields( async(error,values)=>{
+
+  /*
+  添加角色
+   */
+  addRole = () => {
+    // 进行表单验证, 只能通过了才向下处理
+    this.form.validateFields(async (error, values) => {
       if (!error) {
-        //隐藏确认框
-        this.setState({isShowAdd:false})
-        const {roleName} = values
-        //this.form.resetFields()
-        const result = await reqAddRole(roleName)
-        console.log(result,roleName);
-        if (result.status===0) {
-          message.success('添加角色成功')
-          this.getRoles()
-        }else{
-          message.error('添加角色失败')
+        // 隐藏确认框
+        this.setState({
+          isShowAdd: false,
+        });
+
+        // 收集输入数据
+        const { roleName } = values;
+        this.form.resetFields();
+
+        // 请求添加
+        const result = await reqAddRole(roleName);
+        // 根据结果提示/更新列表显示
+        if (result.status === 0) {
+          message.success("添加角色成功");
+          // this.getRoles()
+          // 新产生的角色
+          const role = result.data;
+          // 更新roles状态
+          /*const roles = this.state.roles
+          roles.push(role)
+          this.setState({
+            roles
+          })*/
+
+          // 更新roles状态: 基于原本状态数据更新
+          this.setState((state) => ({
+            roles: [...state.roles, role],
+          }));
+        } else {
+          message.success("添加角色失败");
         }
       }
-    })
+    });
   };
-  handleCancel = () => {
-    this.setState({ isShowAdd: false });
-    this.form.resetFields()
+
+  /*
+  更新角色
+   */
+  updateRole = async () => {
+    // 隐藏确认框
+    this.setState({
+      isShowAuth: false,
+    });
+
+    const role = this.state.role;
+    // 得到最新的menus
+    const menus = this.auth.current.getMenus();
+    role.menus = menus;
+    role.auth_time = Date.now();
+    role.auth_name = memoryUtils.user.username;
+
+    // 请求更新
+    const result = await reqUpdateRole(role);
+    if (result.status === 0) {
+      // this.getRoles()
+      // 如果当前更新的是自己角色的权限, 强制退出
+      if (role._id === memoryUtils.user.role_id) {
+        memoryUtils.user = {};
+        storageUtils.removeUser();
+        this.props.history.replace("/login");
+        message.success("当前用户角色权限成功");
+      } else {
+        message.success("设置角色权限成功");
+        this.setState({
+          roles: [...this.state.roles],
+        });
+      }
+    }
   };
 
   componentWillMount() {
     this.initColumn();
   }
+
   componentDidMount() {
     this.getRoles();
   }
+
   render() {
-    const { roles, role, isShowAdd } = this.state;
+    const { roles, role, isShowAdd, isShowAuth } = this.state;
+
     const title = (
       <span>
         <Button
           type="primary"
-          style={{ marginRight: 10 }}
-          onClick={() => {
-            this.setState({ isShowAdd: true });
-          }}
+          onClick={() => this.setState({ isShowAdd: true })}
         >
           创建角色
-        </Button>
-        <Button type="primary" disabled={!role._id}>
+        </Button>{" "}
+        &nbsp;&nbsp;
+        <Button
+          type="primary"
+          disabled={!role._id}
+          onClick={() => this.setState({ isShowAuth: true })}
+        >
           设置角色权限
         </Button>
       </span>
     );
+
     return (
       <Card title={title}>
         <Table
@@ -117,17 +198,28 @@ export default class Role extends Component {
           }}
           onRow={this.onRow}
         />
+
         <Modal
           title="添加角色"
           visible={isShowAdd}
           onOk={this.addRole}
-          onCancel={this.handleCancel}
+          onCancel={() => {
+            this.setState({ isShowAdd: false });
+            this.form.resetFields();
+          }}
         >
-          <AddForm
-            setForm={(form) => {
-              this.form = form;
-            }}
-          />
+          <AddForm setForm={(form) => (this.form = form)} />
+        </Modal>
+
+        <Modal
+          title="设置角色权限"
+          visible={isShowAuth}
+          onOk={this.updateRole}
+          onCancel={() => {
+            this.setState({ isShowAuth: false });
+          }}
+        >
+          <AuthForm ref={this.auth} role={role} />
         </Modal>
       </Card>
     );
